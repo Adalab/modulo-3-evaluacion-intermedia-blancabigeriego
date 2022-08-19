f479c6fbd0722bcd252e9216a6d2f82fde4a7c01
Blanca <blancabcbt@gmail.com>
2022-08-19 08:58:09 +0200

    paint the list on html

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 
 import '../styles/App.scss';
-import quotes from 'quotes';
+import quotes from '../services/quotes.json';
+import { useState } from 'react';
 
 function App() {
+  //variables de estado
+  const [data, setData] = useState(quotes);
+
+  //pintar
+  const htmlData = data.map((eachQuote, index)=>{
+    return(
+      <li key={index}>{eachQuote.quote} -{eachQuote.character}</li>
+    )
+  });
+
+
   return (
-   <h1>Frases de friends</h1>
-  );
+   <div>
+     <h1>Frases de friends</h1>
+     {htmlData};
+     
+   </div>
+   );
 }
 
 export default App;
